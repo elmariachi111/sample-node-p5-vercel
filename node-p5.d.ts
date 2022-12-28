9e2b3df47ff872a4c0cee66ba5e6134836c40aac
Stefan <stadolf@gmail.com>
2022-12-28 13:25:26 +0100

    uses non jsdom renderer
    Signed-off-by: Stefan <stadolf@gmail.com>

--- node-p5.d.ts
+++ node-p5.d.ts
@@ -1,1 +1,2 @@
 declare module 'node-p5';
+declare module '@mattheath/p5js-node';
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,36 +1,31 @@
 import { readFile } from 'fs/promises';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import p5 from 'node-p5';
+//import p5 from 'node-p5';
+import p5 from '@mattheath/p5js-node';
+import { Readable } from 'stream';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  //const canvas = createCanvas(200, 200)
-  //const ctx = canvas.getContext('2d')
-
-  const callback = async (filename: string) => {
-    const file = await readFile(filename);
+  const callback = async (stream: Readable) => {
+    //const file = await readFile(filename);
     res.setHeader('Content-Type', 'image/png');
     res.status(200);
-    res.write(file);
-    res.end();
+    stream.pipe(res);
   };
 
-  const sketch = (p: any) => {
-    let x = 100;
-    let y = 100;
-    let canvas: any;
-    p.setup = function () {
-      canvas = p.createCanvas(200, 200);
+  new p5((p5: any) => {
+    p5.setup = function () {
+      p5.pixelDensity(1);
+      p5.createCanvas(200, 200, p5.P2D);
+      p5.randomSeed(10);
     };
+    p5.draw = function () {
+      p5.noLoop();
 
-    p.draw = function () {
-      p.noLoop();
-      p.background(0);
-      p.fill(255);
-      p.rect(x, y, 50, 50);
-      //console.log(dataUrl)
-      p.saveCanvas(canvas, 'myCanvas', 'png').then(callback);
+      p5.background(0);
+      p5.fill(255);
+      p5.rect(100, 100, 50, 50);
+
+      callback(p5.canvas.createPNGStream());
     };
-  };
-
-  let p5Instance = p5.createSketch(sketch);
+  });
 }
